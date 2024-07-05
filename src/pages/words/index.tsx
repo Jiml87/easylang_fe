4d18217af1cc9960f125ff66076d6d3c781080f7
Slavik Semenenko <slavik.semenenko.ua@gmail.com>
2024-07-05 14:44:44 -0400

    fix api

--- src/pages/words/index.tsx
+++ src/pages/words/index.tsx
@@ -1,6 +1,10 @@
 // import ListWords from '@/features/ListWords/ListWords';
+import { useFetchWords } from '@/queries/words';
 
 const ListWords = () => {
+  const { data } = useFetchWords();
+  console.log('data', data);
+
   return <div>ListWords</div>;
 };
 
--- src/queries/words.ts
+++ src/queries/words.ts
@@ -2,4 +2,4 @@ import { useQuery } from 'react-query';
 import axios from './axiosInstance';
 
 export const useFetchWords = () =>
-  useQuery('fetchWords', () => axios.get(`/api/v1/words`));
+  useQuery('fetchWords', () => axios.get(`/words`));
