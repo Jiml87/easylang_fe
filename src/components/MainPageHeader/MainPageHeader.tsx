6170a6c06e0c1c86eae6aa0e4659908ea3dff28f
Slavik Semenenko <slavik.semenenko.ua@gmail.com>
2024-10-12 20:45:45 -0400

    add home link

--- src/components/MainPageHeader/MainPageHeader.tsx
+++ src/components/MainPageHeader/MainPageHeader.tsx
@@ -1,11 +1,22 @@
 'use client';
+import Link from 'next/link';
+
 import Logo from '@/components/Logo/Logo';
+import { rootPage } from '@/config/routes';
 
 const MainPageHeader = () => {
   return (
-    <header className="flex justify-between px-4 py-4">
+    <header className="flex items-center justify-between px-4 py-4">
       <div>
         <Logo />
+      </div>
+      <div>
+        <Link
+          href={rootPage.path}
+          className="px-5 py-2 text-slate-600 hover:underline"
+        >
+          Home
+        </Link>
       </div>
     </header>
   );
