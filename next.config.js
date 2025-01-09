49ee3e94395dd1c4c3aff5ace7350a3534f30d41
Slavik Semenenko <slavik.semenenko.ua@gmail.com>
2025-01-09 16:59:32 -0500

    fix: add envs from variravles

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const API_URL = `http://${API_HOST}:8000`;
 const nextConfig = {
   env: {
     OAUTH_GOOGLE_ID: process.env.OAUTH_GOOGLE_ID,
-    OAUTH_GOOGLE_REDIRECT_URL: process.env.OAUTH_GOOGLE_REDIRECT_URL,
-    AUTH_JWT_SECRET: process.env.AUTH_JWT_SECRET,
+    // OAUTH_GOOGLE_REDIRECT_URL: process.env.OAUTH_GOOGLE_REDIRECT_URL,
+    // AUTH_JWT_SECRET: process.env.AUTH_JWT_SECRET,
     API_HOST: API_HOST,
   },
   compress: true,
